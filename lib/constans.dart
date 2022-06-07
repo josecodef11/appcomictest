d66b4ef35de1c51021b8f546fb8a98f49735c2cf
josecodef11 <jise3403@gmail.com>
2022-06-07 09:48:27 -0500

    api

--- lib/constans.dart
+++ lib/constans.dart
@@ -5,10 +5,10 @@ import 'package:dio/dio.dart';
 
 DateTime datey =DateTime.now().subtract(const Duration(days:1)) ; 
 String dateyes = datey.toString().substring(0,10);
-DateTime dates=DateTime.now().subtract(const Duration(days:2));
+DateTime dates=DateTime.now();
 String datez = dates.toString().substring(0,10);
 const api="?api_key=82a1cd52cc96e1e5d2d510dcd7e793a0d243cb1b&format=json";
-String url="https://comicvine.gamespot.com/api/issues/?api_key=82a1cd52cc96e1e5d2d510dcd7e793a0d243cb1b&filter=date_added:$datey|$datez&format=json";
+String url="https://comicvine.gamespot.com/api/issues/?api_key=82a1cd52cc96e1e5d2d510dcd7e793a0d243cb1b&filter=date_added:$dateyes|$datez&format=json";
 
 String baseUrl = url;  
 final kDioOptions = BaseOptions(
